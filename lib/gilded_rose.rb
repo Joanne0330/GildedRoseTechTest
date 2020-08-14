09fbbbb5132015f0866bb9c6364f0309b564a0f9
Joanne Chen <joanne_r_chen@yahoo.com>
2020-08-14 12:29:36 +0100

    added class injection and enable to manipulate the sell_in and quality if pass in an argument of days passed

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,50 +1,51 @@
 class GildedRose
 
-  def initialize(items)
-    @items = items
+  def initialize(item = Item.new)
+    @items = item.items
+    @items_array = []
   end
   
-  def update_quality
+  def update_quality(days_passed)
     @items.each do |item|
       case item.quality
         when < 50    
           case item.name
             when "Aged Brie"
-              if item.sell_in -= 1
-                item.quality += 1
+              if item.sell_in -= days_passed
+                item.quality += days_passed
               end      
             when "Backstage Passes"
-              if item.sell_in -= 1  
+              if item.sell_in -= days_passed 
                 if item.sell_in == 0
                   item.quality == 0
                 elsif item.sell_in > 0 && <= 5
-                  item.quality += 3
+                  item.quality += 3 * days_passed
                 elsif item.sell_in > 5 && <= 10
-                  item.quality += 2
+                  item.quality += 2 * days_passed
                 else
-                  item.quality += 1
+                  item.quality += days_passed
                 end
               end
             when "Sulfuras"
               item.quality = "This is a legendary item. It's timeless!"  
             when "Conjured items"
-              if item.sell_in -= 1
-                item.quality += 2
+              if item.sell_in -= days_passed
+                item.quality += 2 * days_passed
               end
             else
-              if item.sell_in -= 1
+              if item.sell_in -= days_passed
                 if item.sell_in == 0
-                    item.quality -= 2
+                    item.quality -= 2 * days_passed
                 else
-                    item.quality -=1
+                    item.quality -= days_passed
                 end
               end
             end
           else
-             "Item Quality cannot be over 50"    
+             puts "Item Quality cannot be over 50"    
         end
     end
-
+    @items_array << item
 end
 
 #         if item.name != "Aged Brie" && item.name != "Backstage passes" && item.name != "Sulfuras"
@@ -86,15 +87,17 @@ end
 #   end
   
   class Item
-    attr_accessor :name, :sell_in, :quality
+    attr_accessor :name, :sell_in, :quality, :items
   
     def initialize(name, sell_in, quality)
       @name = name
       @sell_in = sell_in
       @quality = quality
+      @items = []
+      strings_of_items
     end
   
-    def to_string
-      "#{@name}, #{@sell_in}, #{@quality}"
+    def strings_of_items
+      @items << "#{@name}, #{@sell_in}, #{@quality}"
     end
   end
