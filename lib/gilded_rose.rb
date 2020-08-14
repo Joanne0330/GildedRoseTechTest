00e42ee34fd212afe4326679e2e8d2c100249991
Joanne Chen <joanne_r_chen@yahoo.com>
2020-08-14 13:41:27 +0100

    tidied up and ready to write some tests

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -8,19 +8,19 @@ class GildedRose
   def update_quality(days_passed)
     @items.each do |item|
       case item.quality
-        when < 50    
+        when item.quality < 50    
           case item.name
             when "Aged Brie"
               item.sell_in -= days_passed
               item.quality += days_passed
-                   
+
             when "Backstage Passes"
               item.sell_in -= days_passed 
                 if item.sell_in <= 0
                   item.quality == 0
-                elsif item.sell_in > 0 && <= 5
+                elsif item.sell_in > 0 && item.sell_in <= 5
                   item.quality += 3 * days_passed
-                elsif item.sell_in > 5 && <= 10
+                elsif item.sell_in > 5 && item.sell_in <= 10
                   item.quality += 2 * days_passed
                 else
                   item.quality += days_passed
@@ -28,6 +28,7 @@ class GildedRose
 
             when "Sulfuras"
               item.quality = "This is a legendary item. It's timeless!"  
+
             when "Conjured items"
               item.sell_in -= days_passed
               item.quality += 2 * days_passed
@@ -41,62 +42,25 @@ class GildedRose
                 end
             end
           else
-             puts "Item Quality cannot be over 50"    
+            puts "Item Quality cannot be over 50"    
         end
-    end
+      end
     @items_array << item
-end
-
-#         if item.name != "Aged Brie" && item.name != "Backstage passes" && item.name != "Sulfuras"
-#           if item.quality > 0
-#               item.quality -= 1
-#           end
-#         else
-#           if item.quality < 50
-#             item.quality += 1
-#             if item.name == "Backstage passes"
-#               if item.sell_in < 11
-#                 item.quality += 1
-#               end
-#             end
-#           end
-#         end
-#         if item.name != "Sulfuras"
-#           item.sell_in -= 1
-#         end
-#         if item.sell_in < 0
-#           if item.name != "Aged Brie"
-#             if item.name != "Backstage passes"
-#               if item.quality > 0
-#                 if item.name != "Sulfuras"
-#                   item.quality -= 1
-#                 end
-#               end
-#             else
-#               item.quality = 0
-#             end
-#           else
-#             if item.quality < 50
-#               item.quality += 1
-#             end
-#           end
-#         end
-#       end
-#     end
-#   end
-  
-  class Item
-    attr_accessor :name, :sell_in, :quality, :items
-  
-    def initialize(name, sell_in, quality)
-      @name = name
-      @sell_in = sell_in
-      @quality = quality
-      @items = []
-      strings_of_items
-    end
-  
-    def strings_of_items
-      @items << "#{@name}, #{@sell_in}, #{@quality}"
-    end
   end
+end
+  
+class Item
+  attr_accessor :name, :sell_in, :quality, :items
+  
+  def initialize(name, sell_in, quality)
+    @name = name
+    @sell_in = sell_in
+    @quality = quality
+    @items = []
+    strings_of_items
+  end
+  
+  def strings_of_items
+    @items << "#{@name}, #{@sell_in}, #{@quality}"
+  end
+end
