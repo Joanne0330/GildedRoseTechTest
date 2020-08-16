3c4c0c6045542953fab7f203018f8d3315fd78a4
Joanne Chen <joanne_r_chen@yahoo.com>
2020-08-17 00:30:04 +0100

    item quality cannot be over 50 test passed

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,5 +1,5 @@
 class Item
-  attr_accessor :items
+  attr_reader :items
   
   def initialize
     @items = []
@@ -24,56 +24,48 @@ class GildedRose
   end
   
   def update_quality
-    @items.each do |i|
-      p i[0]
-      p i[1]
-      p i[2]
-      p 
-    #   case item[2]
-    #     when item[2] < 50    
-    #       case item.name
-    #         when "Aged Brie"
-    #           item.sell_in -= days_passed
-    #           item.quality += days_passed
+    @items.each do |item|
+      if item[2] > 50 
+        item[2] = "Item quality cannot be over 50!"   
+      #     case item.name
+      #       when "Aged Brie"
+      #         item.sell_in -= days_passed
+      #         item.quality += days_passed
 
-    #         when "Backstage Passes"
-    #           item.sell_in -= days_passed 
-    #             if item.sell_in <= 0
-    #               item.quality == 0
-    #             elsif item.sell_in > 0 && item.sell_in <= 5
-    #               item.quality += 3 * days_passed
-    #             elsif item.sell_in > 5 && item.sell_in <= 10
-    #               item.quality += 2 * days_passed
-    #             else
-    #               item.quality += days_passed
-    #             end
+      #       when "Backstage Passes"
+      #         item.sell_in -= days_passed 
+      #           if item.sell_in <= 0
+      #             item.quality == 0
+      #           elsif item.sell_in > 0 && item.sell_in <= 5
+      #             item.quality += 3 * days_passed
+      #           elsif item.sell_in > 5 && item.sell_in <= 10
+      #             item.quality += 2 * days_passed
+      #           else
+      #             item.quality += days_passed
+      #           end
 
-    #         when "Sulfuras"
-    #           item.quality = "This is a legendary item. It's timeless!"  
+      #       when "Sulfuras"
+      #         item.quality = "This is a legendary item. It's timeless!"  
 
-    #         when "Conjured items"
-    #           item.sell_in -= days_passed
-    #           item.quality += 2 * days_passed
+      #       when "Conjured items"
+      #         item.sell_in -= days_passed
+      #         item.quality += 2 * days_passed
             
-    #         else
-              sell_in = (i[1] -= 1)
-    #             if item[1] <= 0
-    #                 item[2] -= 2 * days_passed
-    #             else
-                    quality = (i[2] -= 1)
-    #             end
-    #         end
-    #       else
-    #         puts "Item Quality cannot be over 50"    
-    #     end
-       
-       print [i[0], sell_in, quality]
+      else
+        sell_in = item[1] -= 1
+      #           if item[1] <= 0
+      #               item[2] -= 2 * days_passed
+      #           else
+        quality = item[2] -= 1
+      #           end
+      #       end
+        p item = [item[0], sell_in, quality]
+
+        
       end
+       
+    
+    end
   end
 end
-
-# a = [["ham", 10, 10], ["coffee", 20, 20]]
-# a.each do |b|
-#    print [b[0], b[1] - 1, b[1] - 1 ]
-# end
   
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -8,6 +8,10 @@ describe GildedRose do
       gr = GildedRose.new([["ham", 10, 10], ["coffee", 20, 20]])
       expect(gr.update_quality).to eq [["ham", 9, 9], ["coffee", 19, 19]]
     end
-  end
 
+    it 'shows that quality cannot be over 50' do
+      gr = GildedRose.new([["ham", 10, 55], ["coffee", 20, 20]])
+      expect(gr.update_quality).to eq [["ham", 10, "Item quality cannot be over 50!"], ["coffee", 19, 19] ]
+    end
+  end
 end
